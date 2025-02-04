3d7351bc7f72ddeffc82eba63933766eae53c69e
Lemesle Cyriaque <cyriaque.lemesle@gmail.com>
2025-02-04 17:10:27 +0100

    tiroir anim

--- script.js
+++ script.js
@@ -1,52 +1,17 @@
-// document.querySelector("#myBox").addEventListener("click", function () {
-//   var box = document.querySelector("#myBox");
-//   box.setAttribute(
-//     "color",
-//     "#" + Math.floor(Math.random() * 16777215).toString(16)
-//   );
-// });
+var drawer1 = document.querySelector("#drawer1");
 
-// document
-//   .querySelector("#rightController")
-//   .addEventListener("triggerdown", function () {
-//     var s = document.querySelector("#mySphere");
-//     s.setAttribute(
-//       "color",
-//       "#" + Math.floor(Math.random() * 16777215).toString(16)
-//     );
-//   });
+function render() {
+  drawer1.addEventListener("click", function () {
+    drawer1.setAttribute(
+      "position",
+      "#" + Math.floor(Math.random() * 16777215).toString(16)
+    );
+  });
 
-////
-
-// var scene = document.querySelector("a-scene");
-// var cylinder = document.createElement("a-cylinder");
-// cylinder.setAttribute("color", "#FF9500");
-// cylinder.setAttribute("height", "2");
-// cylinder.setAttribute("radius", "0.75");
-// cylinder.setAttribute("position", "3 3 0");
-// cylinder.setAttribute("class", "collidable");
-// scene.appendChild(cylinder);
-
-// var t = 0;
-// function render() {
-//   t += 0.01;
-//   requestAnimationFrame(render);
-//   cylinder.setAttribute("position", "3 " + (Math.sin(t * 2) + 3) + " 0");
-
-//   cylinder.addEventListener("click", function () {
-//     cylinder.setAttribute(
-//       "color",
-//       "#" + Math.floor(Math.random() * 16777215).toString(16)
-//     );
-//   });
-
-//   document.querySelector("a-box").addEventListener("triggerdown", function () {
-//     cylinder.setAttribute(
-//       "color",
-//       "#" + Math.floor(Math.random() * 16777215).toString(16)
-//     );
-//   });
-// }
+  drawer1.addEventListener("triggerdown", function () {
+    drawer1.setAttribute("position", "-2.6 0 -4.5");
+  });
+}
 // function checkCollision() {
 //   var camera = document.querySelector("[camera]");
 //   var cameraPosition = camera.getAttribute("position");
