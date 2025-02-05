2499921038953c76ecff848bface087fb59064d0
Lemesle Cyriaque <cyriaque.lemesle@gmail.com>
2025-02-05 17:41:04 +0100

    import object very good very nice

--- script.js
+++ script.js
@@ -39,12 +39,39 @@ document.querySelector("#drawer1").addEventListener("click", function () {
   }
 });
 
-document
-  .querySelector("#rightController")
-  .addEventListener("triggerdown", function () {
-    var drawer1 = document.querySelector("#drawer1");
-    moveToPosition(drawer1, { x: -2.6, y: 0, z: -5 });
-  });
+document.querySelector("#drawer2").addEventListener("click", function () {
+  var drawer2 = document.querySelector("#drawer2");
+  if (
+    drawer2.getAttribute("position").x == -2.6 &&
+    drawer2.getAttribute("position").y == 0.5 &&
+    drawer2.getAttribute("position").z == -5
+  ) {
+    moveToPosition(drawer2, { x: -2.6, y: 0.5, z: -4.35 });
+  } else {
+    moveToPosition(drawer2, { x: -2.6, y: 0.5, z: -5 });
+  }
+});
+
+document.querySelector("#drawer3").addEventListener("click", function () {
+  var drawer3 = document.querySelector("#drawer3");
+  if (
+    drawer3.getAttribute("position").x == -2.6 &&
+    drawer3.getAttribute("position").y == 1 &&
+    drawer3.getAttribute("position").z == -5
+  ) {
+    moveToPosition(drawer3, { x: -2.6, y: 1, z: -4.35 });
+  } else {
+    moveToPosition(drawer3, { x: -2.6, y: 1, z: -5 });
+  }
+});
+var scene = document.querySelector("a-scene");
+
+// document
+//   .querySelector("#rightController")
+//   .addEventListener("triggerdown", function () {
+//     var drawer1 = document.querySelector("#drawer1");
+//     moveToPosition(drawer1, { x: -2.6, y: 0, z: -5 });
+//   });
 
 // function checkCollision() {
 //   var camera = document.querySelector("[camera]");
