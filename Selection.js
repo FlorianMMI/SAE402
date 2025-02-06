4146451826519b08098c9254f0f0b4f757b44feb
François-Pierre Lajudie <129268455+Cafipoo@users.noreply.github.com>
2025-02-06 17:57:46 +0100

    Add files via upload

---
+++ Selection.js
@@ -0,0 +1,393 @@
+let data = [
+  { car : [
+    {
+    "Wheel": "Roues",
+    "Engine": "Moteur",
+    "Body": "Carrosserie",
+    "Headlight": "Phare",
+    "Windshield": "Pare-brise",
+    "Door": "Porte",
+    "Seat": "Siège",
+    "Steering Wheel": "Volant",
+    "Pedal": "Pedale",
+    "Rearview Mirror": "Rétroviseur"
+  }
+  ],
+  bookdata : [
+   {
+    "Page": "Page",
+    "Cover": "Couverture",
+    "Title": "Titre",
+    "Author": "Auteur",
+    "Chapter": "Chapitre",
+    "Paragraph": "Paragraphe",
+    "Word": "Mot",
+    "Sentence": "Phrase",
+    "Punctuation": "Ponctuation",
+    "Period": "Point"
+  }
+
+
+  ],
+  computerdata : [
+    {
+    "Screen": "Ecran",
+    "Keyboard": "Clavier",
+    "Mouse": "Souris",
+    "Webcam": "Webcam",
+    "Microphone": "Microphone",
+    "Speaker": "Haut-parleur",
+    "Headphone": "Écouteur",
+    "USB": "USB",
+    "Port": "Port",
+    "Cable": "Câble"
+  }
+],
+}
+]
+// import { data } from "./data.js";
+
+var scene = document.querySelector("a-scene");
+
+
+// Création de la lumière
+var light = document.createElement("a-light");
+light.setAttribute("type", "point");
+light.setAttribute("color", "#FFFFFF");
+light.setAttribute("intensity", "1"); // Ajuste l'intensité pour éviter une trop forte luminosité
+light.setAttribute("distance", "3");
+light.setAttribute("position", "0 1 -2"); // Ajuste la position pour l'effet visuel
+
+
+var computer = document.getElementById("computer-");
+
+computer.addEventListener("mouseenter", function () {
+  computer.setAttribute("animation", {
+    property: "scale",
+    to: "0.05 0.05 0.05",
+    dur: 200
+  });
+  var compIcon = document.createElement("a-entity");
+  compIcon.setAttribute("obj-model", "obj: #loupe-obj; mtl: #loupe-materiaux");
+  compIcon.id = "computer-icon";
+  compIcon.setAttribute("scale", "10 10 10");
+  compIcon.setAttribute("position", "0 4 0");
+
+  var camera = document.querySelector("[camera]");
+  var cameraPosition = camera.getAttribute("position");
+  var compPosition = computer.getAttribute("position");
+
+  var dx = cameraPosition.x - compPosition.x;
+  var dz = cameraPosition.z - compPosition.z;
+  var rotationY = Math.atan2(dx, dz) * (180 / Math.PI) + 45;
+
+  compIcon.setAttribute("rotation", `45 ${rotationY} 0`);
+  computer.appendChild(compIcon);
+});
+
+computer.addEventListener("mouseleave", function () {
+  var compIcon = document.getElementById("computer-icon");
+  if (compIcon) {
+    computer.removeChild(compIcon);
+  }
+  computer.setAttribute("animation", {
+    property: "scale",
+    to: "0.1 0.1 0.1",
+    dur: 200
+  });
+});
+
+computer.addEventListener("click", function () {
+  const existingText = document.querySelector("#vocab");
+  const existingBox = document.querySelector("a-box");
+  const existingTitle = document.querySelector("#a-text-title");
+  if (existingText && existingBox && existingTitle) {
+    scene.removeChild(existingText);
+    scene.removeChild(existingBox);
+    scene.removeChild(existingTitle);
+  }
+
+  const cameraEl = document.querySelector("[camera]");
+  const camWorldPos = new THREE.Vector3();
+  cameraEl.object3D.getWorldPosition(camWorldPos);
+  const forwardDir = new THREE.Vector3();
+  cameraEl.object3D.getWorldDirection(forwardDir);
+
+  const offset = 1;
+  const textPosition = camWorldPos.clone().add(forwardDir.multiplyScalar(offset));
+
+  var camPosition = cameraEl.getAttribute("position");
+  var camRotation = cameraEl.getAttribute("rotation");
+  let dist = 2;
+  let rad = camRotation.y * (Math.PI / 180);
+  let newX = camPosition.x - dist * Math.sin(rad);
+  let newZ = camPosition.z - dist * Math.cos(rad);
+  let newY = camPosition.y; 
+
+  const aText = document.createElement("a-text");
+  const textTitle = document.createElement("a-text");
+  textTitle.id = "a-text-title";
+  textTitle.setAttribute("value", "vocabulary");
+  textTitle.setAttribute("align", "center");
+  textTitle.setAttribute("color", "#FFD700");
+  textTitle.setAttribute("position", `${newX} ${newY + 0.7} ${newZ}`);
+  textTitle.setAttribute("rotation", `0 ${camRotation.y} 0`);
+  textTitle.setAttribute("scale", "0.7 0.7 0.7");
+  scene.appendChild(textTitle);
+
+  let computerObj = data[0].computerdata[0];
+  let lines = [];
+  for (let key in computerObj) {
+    lines.push(key + " --> " + computerObj[key]);
+  }
+  aText.setAttribute("id", "vocab");
+  aText.setAttribute("value", lines.join("\n"));
+  aText.setAttribute("scale", "0.5 0.5 0.5");
+  aText.setAttribute("position", `${newX} ${newY - 0.2} ${newZ}`);
+  aText.setAttribute("align", "center");
+  aText.setAttribute("color", "#000");
+  aText.setAttribute("rotation", `0 ${camRotation.y} 0`);
+
+  const aBox = document.createElement("a-box");
+  aBox.setAttribute("scale", "0.7 0.7 0.7");
+  aBox.setAttribute("color", "#FFF");
+  aBox.setAttribute("width", "3");
+  aBox.setAttribute("height", "3");
+  aBox.setAttribute("depth", "0.001");
+  aBox.setAttribute("rotation", `0 ${camRotation.y} 0`);
+  const boxOffset = 0.1;
+  const boxX = newX - boxOffset * Math.sin(rad);
+  const boxZ = newZ - boxOffset * Math.cos(rad);
+  aBox.setAttribute("position", `${boxX} ${newY} ${boxZ}`);
+
+  scene.appendChild(aText);
+  scene.appendChild(aBox);
+});
+
+// Création de l'entité livre (book)
+var book = document.createElement("a-entity");
+book = document.getElementById("book");
+book.setAttribute("position", "1 0 -4");
+scene.appendChild(book);
+
+book.addEventListener("mouseenter", function () {
+  book.setAttribute("animation", {
+    property: "scale",
+    to: "0.6 0.6 0.6",
+    dur: 200
+  });
+  var eyeIcon = document.createElement("a-entity");
+  eyeIcon.setAttribute("obj-model", "obj: #loupe-obj; mtl: #loupe-materiaux");
+  eyeIcon.id = "eye-icon";
+  eyeIcon.setAttribute("position", "0 0.5 0");
+  
+  var camera = document.querySelector("[camera]");
+  var cameraPosition = camera.getAttribute("position");
+  var bookPosition = book.getAttribute("position");
+
+  var dx = cameraPosition.x - bookPosition.x;
+  var dz = cameraPosition.z - bookPosition.z;
+
+  var rotationY = Math.atan2(dx, dz) * (180 / Math.PI) + 45;
+
+  eyeIcon.setAttribute("rotation", `45 ${rotationY} 0`);
+  book.appendChild(eyeIcon);
+});
+
+book.addEventListener("mouseleave", function () {
+  var eyeIcon = document.getElementById("eye-icon");
+  if (eyeIcon) {
+    book.removeChild(eyeIcon);
+  }
+});
+
+book.addEventListener("mouseleave", function () {
+  book.setAttribute("animation", {
+    property: "scale",
+    to: "0.5 0.5 0.5",
+    dur: 200
+  });
+});
+
+
+
+book.addEventListener("click", function () { 
+
+  const existingText = document.querySelector("#vocab");
+  const existingBox = document.querySelector("a-box");
+  const existingTitle = document.querySelector("#a-text-title");
+  if (existingText && existingBox && existingTitle) {
+    scene.removeChild(existingText);
+    scene.removeChild(existingBox);
+    scene.removeChild(existingTitle);
+  }
+
+  const cameraEl = document.querySelector("[camera]");
+  const camWorldPos = new THREE.Vector3();
+  cameraEl.object3D.getWorldPosition(camWorldPos);
+  const forwardDir = new THREE.Vector3();
+  cameraEl.object3D.getWorldDirection(forwardDir);
+
+  const offset = 1;
+  const textPosition = camWorldPos.clone().add(forwardDir.multiplyScalar(offset));
+
+  var camPosition = cameraEl.getAttribute("position");
+  var camRotation = cameraEl.getAttribute("rotation");
+  let dist = 2;
+  let rad = camRotation.y * (Math.PI / 180);
+  let newX = camPosition.x - dist * Math.sin(rad);
+  let newZ = camPosition.z - dist * Math.cos(rad);
+  let newY = camPosition.y; 
+  
+  const aText = document.createElement("a-text");
+  const textTitle = document.createElement("a-text");
+  textTitle.id = "a-text-title";
+  textTitle.setAttribute("value", "vocabulary");
+  textTitle.setAttribute("align", "center");
+  textTitle.setAttribute("color", "#FFD700");
+  textTitle.setAttribute("position", `${newX} ${newY + 0.7} ${newZ}`);
+  textTitle.setAttribute("rotation", `0 ${camRotation.y} 0`);
+  textTitle.setAttribute("scale", "0.7 0.7 0.7");
+  scene.appendChild(textTitle);
+
+  let bookObj = data[0].bookdata[0];
+  let lines = [];
+  for (let key in bookObj) {
+    lines.push(key + " --> " + bookObj[key]);
+  }
+  aText.setAttribute("id", "vocab");
+  aText.setAttribute("value", lines.join("\n"));
+  aText.setAttribute("scale", "0.5 0.5 0.5");
+  aText.setAttribute("position", `${newX} ${newY - 0.2} ${newZ}`);
+  aText.setAttribute("align", "center");
+  aText.setAttribute("color", "#000");
+  aText.setAttribute("rotation", `0 ${camRotation.y} 0`);
+
+  const aBox = document.createElement("a-box");
+  aBox.setAttribute("scale", "0.7 0.7 0.7");
+  console.log('Ceci est une box ', aBox);
+  aBox.setAttribute("color", "#FFF");
+  aBox.setAttribute("width", "3");
+  aBox.setAttribute("height", "3");
+  aBox.setAttribute("depth", "0.001");
+  aBox.setAttribute("rotation", `0 ${camRotation.y} 0`);
+  const boxOffset = 0.1;
+  const boxX = newX - boxOffset * Math.sin(rad);
+  const boxZ = newZ - boxOffset * Math.cos(rad);
+  aBox.setAttribute("position", `${boxX} ${newY} ${boxZ}`);
+  
+  scene.appendChild(aText);
+  scene.appendChild(aBox);
+});
+
+
+car = document.getElementById("car-");
+
+console.log(car);
+
+car.addEventListener("mouseenter", function () {
+  car.setAttribute("animation", {
+    property: "scale",
+    to: "0.6 0.6 0.6",
+    dur: 200
+  });
+  var carIcon = document.createElement("a-entity");
+  carIcon.setAttribute("obj-model", "obj: #loupe-obj; mtl: #loupe-materiaux");
+  carIcon.id = "car-icon";
+  carIcon.setAttribute("position", "0 0.8 0");
+
+  var camera = document.querySelector("[camera]");
+  var cameraPosition = camera.getAttribute("position");
+  var carPosition = car.getAttribute("position");
+
+  var dx = cameraPosition.x - carPosition.x;
+  var dz = cameraPosition.z - carPosition.z;
+
+  var rotationY = Math.atan2(dx, dz) * (180 / Math.PI) + 45;
+
+  carIcon.setAttribute("rotation", `45 ${rotationY} 0`);
+  car.appendChild(carIcon);
+});
+
+car.addEventListener("mouseleave", function () {
+  var carIcon = document.getElementById("car-icon");
+  if (carIcon) {
+    car.removeChild(carIcon);
+  }
+});
+
+car.addEventListener("mouseleave", function () {
+  car.setAttribute("animation", {
+    property: "scale",
+    to: "0.5 0.5 0.5",
+    dur: 200
+  });
+});
+
+car.addEventListener("click", function () {
+
+  const existingText = document.querySelector("#vocab");
+  const existingBox = document.querySelector("a-box");
+  const existingTitle = document.querySelector("#a-text-title");
+  if (existingText && existingBox && existingTitle) {
+    scene.removeChild(existingText);
+    scene.removeChild(existingBox);
+    scene.removeChild(existingTitle);
+  }
+
+  const cameraEl = document.querySelector("[camera]");
+  const camWorldPos = new THREE.Vector3();
+  cameraEl.object3D.getWorldPosition(camWorldPos);
+  const forwardDir = new THREE.Vector3();
+  cameraEl.object3D.getWorldDirection(forwardDir);
+
+  const offset = 1;
+  const textPosition = camWorldPos.clone().add(forwardDir.multiplyScalar(offset));
+
+  var camPosition = cameraEl.getAttribute("position");
+  var camRotation = cameraEl.getAttribute("rotation");
+  let dist = 2;
+  let rad = camRotation.y * (Math.PI / 180);
+  let newX = camPosition.x - dist * Math.sin(rad);
+  let newZ = camPosition.z - dist * Math.cos(rad);
+  let newY = camPosition.y; 
+
+  const aText = document.createElement("a-text");
+  const textTitle = document.createElement("a-text");
+  textTitle.id = "a-text-title";
+  textTitle.setAttribute("value", "vocabulary");
+  textTitle.setAttribute("align", "center");
+  textTitle.setAttribute("color", "#FFD700");
+  textTitle.setAttribute("position", `${newX} ${newY + 0.7} ${newZ}`);
+  textTitle.setAttribute("rotation", `0 ${camRotation.y} 0`);
+  textTitle.setAttribute("scale", "0.7 0.7 0.7");
+  scene.appendChild(textTitle);
+
+  let carObj = data[0].car[0];
+  let lines = [];
+  for (let key in carObj) {
+    lines.push(key + " --> " + carObj[key]);
+  }
+  aText.setAttribute("id", "vocab");
+  aText.setAttribute("value", lines.join("\n"));
+  aText.setAttribute("scale", "0.5 0.5 0.5");
+  aText.setAttribute("position", `${newX} ${newY - 0.2} ${newZ}`);
+  aText.setAttribute("align", "center");
+  aText.setAttribute("color", "#000");
+  aText.setAttribute("rotation", `0 ${camRotation.y} 0`);
+
+  const aBox = document.createElement("a-box");
+  aBox.setAttribute("scale", "0.7 0.7 0.7");
+  aBox.setAttribute("color", "#FFF");
+  aBox.setAttribute("width", "3");
+  aBox.setAttribute("height", "3");
+  aBox.setAttribute("depth", "0.001");
+  aBox.setAttribute("rotation", `0 ${camRotation.y} 0`);
+  const boxOffset = 0.1;
+  const boxX = newX - boxOffset * Math.sin(rad);
+  const boxZ = newZ - boxOffset * Math.cos(rad);
+  aBox.setAttribute("position", `${boxX} ${newY} ${boxZ}`);
+  
+  scene.appendChild(aText);
+  scene.appendChild(aBox);
+});
---
+++ script.js
@@ -0,0 +1,121 @@
+AFRAME.registerComponent("thumbstick-move", {
+  init: function () {
+    let rig = document.getElementById("rig");
+    let camera = document.getElementById("camera");
+
+    this.el.addEventListener("thumbstickmoved", function (evt) {
+      let x = evt.detail.x; // Gauche/Droite
+      let y = evt.detail.y; // Avant/Arrière
+
+      if (Math.abs(x) < 0.1 && Math.abs(y) < 0.1) return; // Évite les petits mouvements parasites
+
+      let speed = 0.1; // Vitesse de déplacement augmentée
+
+      // Récupère la direction de la caméra
+      let direction = new THREE.Vector3();
+      camera.object3D.getWorldDirection(direction);
+      direction.y = 0; // Ignore la hauteur pour éviter le mouvement vertical
+      direction.normalize();
+
+      // Calcul du mouvement
+      let strafe = new THREE.Vector3()
+        .crossVectors(new THREE.Vector3(0, 1, 0), direction)
+        .multiplyScalar(x);
+      let move = direction.multiplyScalar(y); // On garde y sans inverser cette fois
+
+      let finalMove = new THREE.Vector3()
+        .addVectors(strafe, move)
+        .multiplyScalar(speed);
+
+      // Appliquer le mouvement
+      rig.object3D.position.add(finalMove);
+    });
+  },
+});
+
+function moveToPosition(object, targetPosition) {
+  var currentPosition = object.getAttribute("position");
+  var step = 0.01;
+
+  function animate() {
+    var dx = targetPosition.x - currentPosition.x;
+    var dy = targetPosition.y - currentPosition.y;
+    var dz = targetPosition.z - currentPosition.z;
+
+    var distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+    if (distance < step) {
+      object.setAttribute("position", targetPosition);
+      return;
+    }
+
+    currentPosition.x += (dx * step) / distance;
+    currentPosition.y += (dy * step) / distance;
+    currentPosition.z += (dz * step) / distance;
+
+    object.setAttribute("position", currentPosition);
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+}
+
+document.querySelector("#drawer1").addEventListener("click", function () {
+  var drawer1 = document.querySelector("#drawer1");
+  if (
+    drawer1.getAttribute("position").x == -2.6 &&
+    drawer1.getAttribute("position").y == 0 &&
+    drawer1.getAttribute("position").z == -5
+  ) {
+    moveToPosition(drawer1, { x: -2.6, y: 0, z: -4.35 });
+  } else {
+    moveToPosition(drawer1, { x: -2.6, y: 0, z: -5 });
+  }
+});
+
+document.querySelector("#drawer2").addEventListener("click", function () {
+  var drawer2 = document.querySelector("#drawer2");
+  if (
+    drawer2.getAttribute("position").x == -2.6 &&
+    drawer2.getAttribute("position").y == 0.5 &&
+    drawer2.getAttribute("position").z == -5
+  ) {
+    moveToPosition(drawer2, { x: -2.6, y: 0.5, z: -4.35 });
+  } else {
+    moveToPosition(drawer2, { x: -2.6, y: 0.5, z: -5 });
+  }
+});
+
+document.querySelector("#drawer3").addEventListener("click", function () {
+  var drawer3 = document.querySelector("#drawer3");
+  if (
+    drawer3.getAttribute("position").x == -2.6 &&
+    drawer3.getAttribute("position").y == 1 &&
+    drawer3.getAttribute("position").z == -5
+  ) {
+    moveToPosition(drawer3, { x: -2.6, y: 1, z: -4.35 });
+  } else {
+    moveToPosition(drawer3, { x: -2.6, y: 1, z: -5 });
+  }
+});
+
+let renderCharacter = function (){
+  let ascene = document.querySelector('a-scene');
+  let acharacter = document.createElement('a-entity');
+  // let character = document.getElementById('character');
+  acharacter.setAttribute('gltf-model', '#character');
+  acharacter.setAttribute('scale', '1.75 1.75 1.75');
+  acharacter.setAttribute('position', '-2 0 -8');
+  acharacter.setAttribute('id', 'characters');
+  acharacter.setAttribute('animation-mixer', 'clip: CharacterArmature|Wave; loop: repeat; timeScale: 1');
+  ascene.appendChild(acharacter);
+
+  acharacter.addEventListener("click", function () {
+      console.log("click");
+      let anim = acharacter.getAttribute('animation-mixer');
+      console.log(anim);
+      acharacter.setAttribute('animation-mixer', 'clip: CharacterArmature|Death; loop: repeat; timeScale: 1');
+    });
+};
+renderCharacter ();
