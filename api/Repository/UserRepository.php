e34426df02ebf6d287693ca57ae432442d22f060
FlorianMMI <florian.bounissou@etu.unlim.fr>
2025-02-19 14:47:38 +0100

    Fix SQL query in UserRepository and add CORS configuration in .htaccess

--- api/Repository/UserRepository.php
+++ api/Repository/UserRepository.php
@@ -17,20 +17,20 @@ class UserRepository extends EntityRepository {
     }
 
     public function finduser($user) {
-        $requete = $this->cnx->prepare("SELECT * FROM User WHERE players_names = :user");
+        $requete = $this->cnx->prepare("SELECT * FROM User WHERE players_name = :user");
         $requete->bindParam(':user', $user);
         $requete->execute();
         $answer = $requete->fetchAll(PDO::FETCH_CLASS);
         return $answer;
     }
 
-    // public function finditems($user) {
-    //     $requete = $this->cnx->prepare("SELECT shop.achat FROM shop INNER JOIN User ON shop.id_user = User.id_user WHERE User.players_name = :user");
-    //     $requete->bindParam(':user', $user);
-    //     $requete->execute();
-    //     $answer = $requete->fetchAll(PDO::FETCH_CLASS);
-    //     return $answer;
-    // }
+    public function finditems($user) {
+        $requete = $this->cnx->prepare("SELECT shop.achat FROM shop INNER JOIN User ON shop.id_user = User.id_user WHERE User.players_name = :user");
+        $requete->bindParam(':user', $user);
+        $requete->execute();
+        $answer = $requete->fetchAll(PDO::FETCH_CLASS);
+        return $answer;
+    }
 
 }
 
